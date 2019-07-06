226dd443d7df26fafac83c6a7c0cd049e394fd27
Zachary Smith <intensestuffs@gmail.com>
2019-07-06 00:43:43 -0500

    Add links to home pages of controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: 'hello, world!'
+    render html: 'I love Pogue, just kidding'
   end
   def goodbye 
     render html: 'goodbye, world!'
